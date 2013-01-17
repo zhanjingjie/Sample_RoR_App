973d5da3c8e534671eaafc561181668e09aacc89
root <root@saasbook.(none)>
2013-01-17 10:03:36 -0500

    Add contact page. Also refactor the RSpec test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +13,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | Home")
+				:text => "#{base_title} | Home")
     end
   end
 
@@ -22,7 +25,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do 
       visit '/static_pages/help'
-      page.should have_selector('title', :text => '| Help')
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -34,7 +37,19 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => 'About Us')
+      page.should have_selector('title', :text => "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should haave the h1 'Contact'" do 
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 end
