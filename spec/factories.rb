ba3ce9851085774db66ea2fbc1b765fd7e38d289
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-01-21 00:10:15 +0800

    Add FactoryGirl gem. Modify the user profile page by using Gravatar image.

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name  "Jingjie Zhan"
+    email "zhanjingjie.jj@gmail.com"
+    password "zjj.19910924"
+    password_confirmation "zjj.19910924"
+  end
+end
