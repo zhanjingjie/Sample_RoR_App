6c0a574f2f12b6d966a29ff3f5b1b73d4452957c
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-01-24 20:48:11 -0800

    Add remember_token attr to user model. Include SessionHelper in all controller to persist the cookie. Add flash message for invalid signin.:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			# Sign the user in and redirect to the user's show page
+			sign_in user # A helper function
+			redirect_to user
+		else 
+			# will pass the flash variable to the view
+			flash.now[:error] = 'Invalid email/password combination' # Not quite right
+			render 'new'
+		end
 	end
 
 	def destroy
