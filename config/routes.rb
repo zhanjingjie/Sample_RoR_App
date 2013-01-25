ee17797ddcefe88cbfffbc17074630287ff678fc
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-01-24 19:40:48 -0800

    Create sessions controller. Add three basic methods to sessions controller. Construct basic view to pass the minimal test.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 SampleApp::Application.routes.draw do
+  # Easy way to generate RESTful behavior (action)
   resources :users
+  resources :sessions, only: [ :new, :create, :destroy ]
 
-  root to: 'static_pages#home'
-
+  # customize the routes' name
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
+  # static pages routes. eg. help_path is for action help. No need for RESTful actions.
+  root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
-
   match '/about', to: 'static_pages#about'
-
   match '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
