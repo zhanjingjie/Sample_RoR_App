51dea60ca5aefd7365084b6e2a5b8c228e7ea228
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-02-19 16:55:46 -0800

    Eliminate the bug failing the edit page test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,24 @@ describe "UserPages" do
  
   subject { page }
 
+  describe "edit" do
+    let(:user) {FactoryGirl.create(:user)}
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1', text:"Update your profile") }
+      it { should have_selector('title', text:"Edit user") }
+      it { should have_link('change', href:'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+  end
+
+
+
   describe "Signup page" do
     before { visit signup_path }
     it { should have_selector('h1', text: 'Sign Up') }
@@ -54,4 +72,5 @@ describe "UserPages" do
       end
     end
   end
+
 end
