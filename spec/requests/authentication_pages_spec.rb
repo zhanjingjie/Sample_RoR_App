7ab30e17f11142c0c7a78276ee6a09fdd406f876
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-02-22 14:36:46 -0800

    Connect settings with edit page. Add test for settings link after login. Add helper method sign-in for testing.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,9 +28,10 @@ describe "Authentication" do
   		describe "with valid information" do
   			let(:user) { FactoryGirl.create(:user) }
         # The helper method is in spec utilities
-  			before { valid_signin(user) }
+  			before { sign_in user }
   			it { should have_selector('title', text: user.name) }
   			it { should have_link('Profile', href: user_path(user)) }
+        it { should have_link('Settings', href: edit_user_path(user)) }
   			it { should have_link('Sign out', href: signout_path) }
   			it { should_not have_link('Sign in', href: signin_path) }
 
