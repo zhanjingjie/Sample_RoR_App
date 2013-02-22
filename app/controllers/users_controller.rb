f8f053f9e7ff3d0bfebab9c8438a26557bee6a4e
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-02-22 15:09:55 -0800

    Finish update action in User action. Add tests for valid edits.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,5 +22,17 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else 
+      render 'edit'
+    end
+  end
   
 end
