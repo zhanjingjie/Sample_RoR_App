7eafa166c5ba4ff64e2d2f32966330f943e9f6d7
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-02-22 15:54:56 -0800

    Add before_filter for update and edit actions. Add tests for checking if an unauthorized user can access others profiles, should be redirected to signin page.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -2,6 +2,21 @@ require 'spec_helper'
 
 describe "Authentication" do
 	subject { page }
+
+  describe "authorization" do
+    let(:user) {FactoryGirl.create(:user)}
+    describe "in the Users controller" do
+      describe "visiting the edit page" do
+        before { visit edit_user_path(user) }
+        it { should have_selector('title', text: 'Sign in') }
+      end
+      describe "submitting to the update action" do
+        before { put user_path(user) }
+        specify { response.should redirect_to(signin_path) }
+      end
+    end
+  end
+
 	describe "signin page" do 
 		before { visit signin_path }
 		it { should have_selector('h1', text: 'Sign in') }
