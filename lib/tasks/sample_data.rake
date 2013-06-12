0db51593f6eedaa584e562967d5491b91c51aa6f
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-06-11 18:15:12 -0700

    Add delte function for admin user.

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,10 +1,12 @@
 namespace :db do
 	desc "Fill database with sample data"
 	task populate: :environment do
-		User.create!(name: "Example User", 
+		admin = User.create!(name: "Example User", 
 					 email: "example@railstutorial.org",
 					 password: "foobar", 
 					 password_confirmation: "foobar")
+		# Make the first generated user admin by default
+		admin.toggle!(:admin)
 		99.times do |n|
 			name = Faker::Name.name
 			email = "example-#{n+1}@railstutorial.org"
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,8 +16,20 @@ describe "Authentication" do
       end
     end
 
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+
       describe "in the Users controller" do
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
